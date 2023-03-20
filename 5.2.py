6a7de266aab2e320b8d9eb0c0ff5e95f289fdc6a
monstercat16 <tsygankov1357@gmail.com>
2023-03-21 01:49:04 +0300

    test-test

--- 5.2.py
+++ 5.2.py
@@ -1,5 +1,8 @@
-from random import randint
-
-lst = [randint(0, 10) for i in range(10)]
-print(*lst)
-print(*set([i for index, i in enumerate(lst) if i in lst[index + 1:]]))
+numbers = [1, 2, 3, 4, 5]
+if len(set(numbers)) != len(numbers):
+    print("В списке есть повторяющиеся элементы")
+    for number in set(numbers):
+        if numbers.count(number) > 1:
+            print(number)
+else:
+    print("В списке нет повторяющихся элементов")
